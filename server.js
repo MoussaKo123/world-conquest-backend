f5ea0e4d4be563292bcf2edf06ed2885ebe76b6d
MoussaKo123 <u2609669658@gmail.com>
2026-03-15 16:54:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // ═══════════════════════════════════════════════════════
 
 const express = require('express');
-const stripe = require('stripe')('sk_test_TON_SECRET_ICI'); // Remplace par ta nouvelle clé secrète
+const stripe = const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const cors = require('cors');
 
 const app = express();
